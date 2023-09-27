d0cf55210a1284abedf27af74657c583cbed45cb
ProUzzal <uzzalahmed0001@gmail.com>
2023-09-27 17:30:21 +0600

    fixed design problem, add file to local storage and fix others some bugs

--- script.js
+++ script.js
@@ -6,75 +6,157 @@
 let inputedTask = document.querySelector('input[type="text"]');
 let form = document.querySelector('form');
 let addBtn = document.querySelector('input[type="submit"]');
-let taskListUL = document.querySelector('.task-list>ul')
+let taskListUL = document.querySelector('.task-list>ul');
+// let li = document.querySelector('.task-list>ul li.checked')
 
 
 
+//clear all items
+document.querySelector('.clear-btn').addEventListener('click', () => {
+    localStorage.clear();
+    taskListUL.innerHTML = '';
+})
 
-//toogleCheckOrUncheck
 
-const taskStatusCheck = function () {
+'I have to complete this task in sha allah'
+//delete single items
+const deleteSingleItem = function (deleteBtnImg, task) {
+    console.log(deleteBtnImg);
+    const taskArr = getTaskArr();
+    // console.log(taskArr);
 
-    taskListUL.addEventListener('click', function (e) {
-        console.log(e.target.classList.toggle('checked'));
-        let target = e.target;
+    deleteBtnImg.addEventListener('click', () => {
 
-        if (target.classList.contains('checked')) {
-            target.classList.toggle('');
+        if (taskArr.indexOf(task) != -1) {
 
+            taskArr.splice(taskArr.indexOf(task), 1);
+
+            // console.log(task.parentNode);
+
+            // console.log(taskArr);
+            localStorage.setItem('allTasks', JSON.stringify(taskArr));
+
+            taskDisplay();
         }
+    }, false)
 
 
-    },{})
+}
+
+
+//toogleTask
+const controlTask = function () {
+    const li = document.querySelector('.task-list>ul li');
+    const images = document.querySelectorAll('.uncheck');
+    // console.log(images);
+    images.forEach((image) => {
+        image.addEventListener('click', (e) => {
+            const targetedElement = e.target;
+            if (targetedElement.classList.contains('uncheck')) {
+                targetedElement.classList.remove('uncheck');
+                targetedElement.classList.add('check');
+                targetedElement.src = 'assets/check-circle.png'
+
+                targetedElement.nextSibling.style.textDecoration = 'line-through';
+            }
+            else {
+                targetedElement.classList.remove('check');
+                targetedElement.classList.add('uncheck');
+                targetedElement.nextSibling.style.textDecoration = 'none';
+                targetedElement.src = 'assets/unchecked.png'
+            }
+
+        })
+    })
+
+
 }
 
 
 
-// Function to display tasks on the page
+
+// Function to display okay here.
 const taskDisplay = function () {
-    // taskListUL.innerHTML = '';
-    const allTask = getTaskArr();
-    // console.log(tasks);
-    allTask.forEach((t, i) => {
+    taskListUL.innerHTML = '';
+    const allTaskObj = getTaskArr();
+
+    allTaskObj.forEach((taskObj) => {
         const li = document.createElement('li');
-        li.innerHTML = t;
+        const checkImg = document.createElement('img');
+        const span = document.createElement('span');
+        span.innerHTML = taskObj.taskText;
+        const deleteImg = document.createElement('img');
+        if (taskObj.status === 'checked') {
+            checkImg.className = 'check';
+            checkImg.src = 'assets/check-circle.png';
+            span.style.textDecoration = 'line-through';
+        }
+        else {
+            checkImg.className = 'uncheck';
+            checkImg.src = 'assets/unchecked.png';
+        }
+        // span.innerHTML = taskObj.text;
+        deleteImg.src = "assets/delete-outline.png";
+        deleteImg.alt = '';
+        deleteImg.style.cursor = 'pointer';
+
+        li.appendChild(checkImg);
+        li.appendChild(span);
+        li.appendChild(deleteImg);
         taskListUL.appendChild(li);
+        deleteSingleItem(deleteImg);
 
-        taskStatusCheck();
+         // Set up event listener to toggle task status when clicked
+        checkImg.addEventListener('click', () => {
+            taskObj.status = (taskObj.status === 'checked') ? 'unchecked' : 'checked';
+            taskSave(allTaskObj); // Save updated task status to local storage
+            taskDisplay(); // Refresh the displayed task list
+            
+        })
+    })
+    controlTask()
 
-    },)
 }
 
-// Initial display of tasks
-// displayTasks();
+
+
+const createNewTaskObj = function (task) {
+    return { taskText: task, status: 'unchecked' }
+}
 
 //Task storing in local Storage-3
-const taskSave = function (arrOfTask) {
-    localStorage.setItem('allTasks', JSON.stringify(arrOfTask));
+const taskSave = function (arrOfTaskObj) {
+    // console.log(arrOfTask);
+    localStorage.setItem('allTasks', JSON.stringify(arrOfTaskObj));
 }
 // Function to retrieve tasks from local storage-2
 const getTaskArr = function () {
-    console.log(localStorage.getItem('allTasks'));
+    // console.log(localStorage.getItem('allTasks'));
     return JSON.parse(localStorage.getItem('allTasks')) || [];
 }
+
+
+
 
 // handle task submission form-1
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
-    const taskText = inputedTask.value.trim();
+    const inputedTaskText = inputedTask.value.trim();
 
-    if (taskText === '') {
+    if (inputedTaskText === '') {
         alert('You have to enter a task');
         return;
     }
-
     //Here we will get back an empty array or filled array from getTaskArray
     const tasksArr = getTaskArr();
-    tasksArr.push(taskText);
+    const taskObj = createNewTaskObj(inputedTaskText);
+    tasksArr.push(taskObj);
     taskSave(tasksArr);
     inputedTask.value = '';
     taskDisplay();
+
 });
 
 
 
+window.onload = taskDisplay;
